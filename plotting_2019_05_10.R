88983ee2c1854bfa904b90b99d0f5df1a2555d72
beguma <beguma@biomim-mbp.local>
2019-05-10 17:25:09 -0400

    plotting

--- plotting_2019_05_10.R
+++ plotting_2019_05_10.R
@@ -4,6 +4,47 @@ library(ggplot2)
 library(scales)
 
 
+data.plot <- all_params %>%
+  select(fold.change.param, 
+         #AFIR_thy, 
+         SCIM_thy_ketl_neg,
+         #SCIM_thy_ketl_pos,
+         SCIM.sim, 
+         SCIM.thy,
+         #TL0.sim,
+         #TL0.thy,
+         #TL0_neg.thy,
+         #TL0_pos.thy,
+         drug, param) %>%
+  gather(key, value, -c(fold.change.param,drug,param))
+
+g <- ggplot(data.plot, aes(x=fold.change.param, y=value, color=key, linetype=key)) + 
+  facet_grid(drug ~ param, scale = "free_y", switch = "y") + 
+  geom_line(size=1, alpha=0.6) +
+  scale_x_log10() + 
+  scale_y_log10() +
+  scale_color_manual(values = c(#SCIM_thy_ketl_pos="orange",
+                               TL0.sim="black",
+                               TL0.thy="red",
+                               TL0_neg.thy="green"))
+                               #TL0_pos.thy="blue"
+print(g)
+
+g <- ggplot(data.plot, aes(x=fold.change.param, y=value, color=key, linetype=key)) + 
+  facet_grid(drug ~ param, scale = "free_y", switch = "y") + 
+  geom_line(size=1, alpha=0.6) +
+  scale_x_log10() + 
+  scale_y_log10() +
+  scale_color_manual(values = c(#SCIM_thy_ketl_pos="blue", 
+                                SCIM_thy_ketl_neg="green4",
+                                SCIM.sim="black",
+                                SCIM.thy="red"))
+
+print(g)
+
+
+
+
 g <- ggplot(params, aes(x=fold.change.param)) + 
   facet_grid(drug ~ param,scales = "free_y") + 
   geom_line(aes(y = AFIR_thy),color = "green",size = 1) +
